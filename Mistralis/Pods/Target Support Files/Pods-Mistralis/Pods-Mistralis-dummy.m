cdfd849d51d97325df57f5006f7f7b65f467fb04
Petros Dhespollari <info@peterdsp.dev>
2025-03-25 20:21:32 +0200

    Changes from Mistralis to PromptBar

--- Mistralis/Pods/Target Support Files/Pods-Mistralis/Pods-Mistralis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mistralis : NSObject
-@end
-@implementation PodsDummy_Pods_Mistralis
-@end
---
+++ PromptBar/PromptBar/Helpers/ObservableObject.swift
@@ -0,0 +1,12 @@
+//
+//  ObservableObject.swift
+//  PromptBar
+//
+//  Created by Petros Dhespollari on 17/3/24.
+//
+
+import SwiftUI
+
+class ReloadState: ObservableObject {
+    @Published var shouldReload: Bool = false
+}
